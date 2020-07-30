86341bece25d6741d4314fe2fddfa65d95c9afc4
Haroon Rafique <haroon.rafique@protonmail.com>
2020-07-30 13:12:34 +0200

    01: testing to fix MAD-X bug

--- 01_Generate_Initial_Distribution/0_H_21/pyOrbit.py
+++ 01_Generate_Initial_Distribution/0_H_21/pyOrbit.py
@@ -117,7 +117,7 @@ else:
 # Write tunes.str file for MAD-X input
 #-----------------------------------------------------------------------
 if not rank:
-        script_name = 'tunes.str'
+        script_name = 'PS_Lattice/tunes.str'
         if os.path.exists(script_name):  
                 print 'tune file ' + script_name + ' already exists. Deleting'
                 os.remove(script_name)
