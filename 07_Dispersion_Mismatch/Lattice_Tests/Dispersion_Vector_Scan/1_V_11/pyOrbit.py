0a419cd6cccf0ef4e1344901cbeb2c61f1906127
Haroon Rafique <haroon.rafique@protonmail.com>
2020-08-21 13:36:40 +0200

    07: Vertical dispersion vector scan bugfix

--- 07_Dispersion_Mismatch/Lattice_Tests/Dispersion_Vector_Scan/1_V_11/pyOrbit.py
+++ 07_Dispersion_Mismatch/Lattice_Tests/Dispersion_Vector_Scan/1_V_11/pyOrbit.py
@@ -248,14 +248,10 @@ if sts['turn'] < 0:
 		Dx = 0.
 		Dxp = 0.
 		if s['Dispersion_Mismatch']:
-                        if p['transverse_plane_flag']:
-                                Delta_D, D_M = find_dispersion_mismatch(Lattice.etax0, Lattice.betax0, M_D=0.35, verbose=True)
-                        else:
-                                Delta_D, D_M = find_dispersion_mismatch(Lattice.etax0, Lattice.betay0, M_D=0.35, verbose=True)
-                        
+                        Delta_D, D_M = find_dispersion_mismatch(Lattice.etax0, Lattice.betax0, M_D=0.35, verbose=True)
                         twiss_dict['D_x'] = D_M
-			Dx  = D_M
-			Dxp = Lattice.etapx0
+                        Dx  = D_M
+                        Dxp = Lattice.etapx0
                 else:
 			Dx  = Lattice.etax0
 			Dxp = Lattice.etapx0
