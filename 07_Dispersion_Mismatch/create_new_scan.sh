0417a9c7d1d7dbbe3466fe8c3037efaa937dd782
Haroon Rafique <haroon.rafique@protonmail.com>
2020-08-21 15:33:38 +0200

    07: Dispersion mismatch scan added

---
+++ 07_Dispersion_Mismatch/create_new_scan.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+folder_in=00_Master
+
+cp -r ${folder_in} 1_H_07
+cp -r ${folder_in} 1_H_08
+cp -r ${folder_in} 1_H_09
+cp -r ${folder_in} 1_H_10
+cp -r ${folder_in} 1_H_11
+cp -r ${folder_in} 1_H_12
+cp -r ${folder_in} 1_H_13
+cp -r ${folder_in} 1_H_14
+cp -r ${folder_in} 1_H_15
+cp -r ${folder_in} 1_H_16
+cp -r ${folder_in} 1_H_17
+cp -r ${folder_in} 1_H_18
+cp -r ${folder_in} 1_H_19
+cp -r ${folder_in} 1_H_20
+cp -r ${folder_in} 1_H_21
+
+cp -r ${folder_in} 1_V_10
+cp -r ${folder_in} 1_V_11
+cp -r ${folder_in} 1_V_12
+cp -r ${folder_in} 1_V_13
+cp -r ${folder_in} 1_V_14
+cp -r ${folder_in} 1_V_15
+cp -r ${folder_in} 1_V_16
+cp -r ${folder_in} 1_V_17
+cp -r ${folder_in} 1_V_18
+cp -r ${folder_in} 1_V_19
+cp -r ${folder_in} 1_V_20
+cp -r ${folder_in} 1_V_21
+cp -r ${folder_in} 1_V_22
+cp -r ${folder_in} 1_V_23
+cp -r ${folder_in} 1_V_24
+
+cp -r ${folder_in} 0_H_07
+cp -r ${folder_in} 0_H_08
+cp -r ${folder_in} 0_H_09
+cp -r ${folder_in} 0_H_10
+cp -r ${folder_in} 0_H_11
+cp -r ${folder_in} 0_H_12
+cp -r ${folder_in} 0_H_13
+cp -r ${folder_in} 0_H_14
+cp -r ${folder_in} 0_H_15
+cp -r ${folder_in} 0_H_16
+cp -r ${folder_in} 0_H_17
+cp -r ${folder_in} 0_H_18
+cp -r ${folder_in} 0_H_19
+cp -r ${folder_in} 0_H_20
+cp -r ${folder_in} 0_H_21
+
+cp -r ${folder_in} 0_V_10
+cp -r ${folder_in} 0_V_11
+cp -r ${folder_in} 0_V_12
+cp -r ${folder_in} 0_V_13
+cp -r ${folder_in} 0_V_14
+cp -r ${folder_in} 0_V_15
+cp -r ${folder_in} 0_V_16
+cp -r ${folder_in} 0_V_17
+cp -r ${folder_in} 0_V_18
+cp -r ${folder_in} 0_V_19
+cp -r ${folder_in} 0_V_20
+cp -r ${folder_in} 0_V_21
+cp -r ${folder_in} 0_V_22
+cp -r ${folder_in} 0_V_23
+cp -r ${folder_in} 0_V_24
