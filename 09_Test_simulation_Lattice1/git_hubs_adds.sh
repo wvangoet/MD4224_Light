b58418a3d79cc9197d2d5877e493f9d6fdbed9e8
Wietse Van Goethem <wvangoet@slurmgate02.cern.ch>
2020-09-24 15:16:00 +0200

    data transfer

---
+++ 09_Test_simulation_Lattice1/git_hubs_adds.sh
@@ -0,0 +1,37 @@
+git add 0_H_21/bunch_output/*
+git add	0_H_19/bunch_output/*
+git add	0_H_17/bunch_output/*
+git add	0_H_15/bunch_output/*
+git add	0_H_13/bunch_output/*
+git add	0_H_11/bunch_output/*
+git add	0_H_09/bunch_output/*
+git add	0_H_07/bunch_output/*
+
+git add	1_H_21/bunch_output/*
+git add 1_H_19/bunch_output/*
+git add 1_H_17/bunch_output/*
+git add 1_H_15/bunch_output/*
+git add 1_H_13/bunch_output/*
+git add 1_H_11/bunch_output/*
+git add 1_H_09/bunch_output/*
+git add 1_H_07/bunch_output/*
+
+git add 0_V_10/bunch_output/*
+git add	0_V_12/bunch_output/*
+git add	0_V_14/bunch_output/*
+git add	0_V_16/bunch_output/*
+git add	0_V_18/bunch_output/*
+git add	0_V_20/bunch_output/*
+git add	0_V_22/bunch_output/*
+git add	0_V_24/bunch_output/*
+
+git add	1_V_10/bunch_output/*
+git add 1_V_12/bunch_output/*
+git add 1_V_14/bunch_output/*
+git add 1_V_16/bunch_output/*
+git add 1_V_18/bunch_output/*
+git add 1_V_20/bunch_output/*
+git add 1_V_22/bunch_output/*
+git add 1_V_24/bunch_output/*
+
+
